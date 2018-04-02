3acca6c46da30e40d98204dcf3bc4a30dd1ecae0
DESKTOP-2B0IMKH\sj <dbtjdwocjswo>
2018-04-02 16:05:52 +0900

    세션로그인 관리 ~ 사용자 인증 단계 공부후 추가 개발

--- testsql/views.py
+++ testsql/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import SUser
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from . import dao
 from django.contrib import messages
@@ -17,15 +17,14 @@ def loginuser(request):
     userid = request.POST.get('UserID')
     userpw = request.POST.get('UserPW')
     for val in alluser:
-        #아이디와 비밀번호가 맞을 경우, userMain이라는 페이지 생성
+        #아이디와 비밀번호가 맞을 경우, userMain이라는 페이지 이동
         if userid==val["id"] and userpw==val["pw"]:
              return render(request,"userMain.html",{
                  "userid" : userid,
-                 "userpw" : userpw
+                 "userpw" : userpw,
              })
         #아이디와 비밀번호가 맞지 않을경우
-        else:
-             
+        else:             
              return render(request,"Main.html")
 
 def registuser(request):
